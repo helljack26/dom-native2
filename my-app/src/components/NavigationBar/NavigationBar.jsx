3d180282a4a0fab80f1b278569e9484ed0c20fc5
helljack26 <helljack22@gmail.com>
2022-04-30 14:24:34 +0300

    make reset catalog when press home on tabNavigator

--- my-app/src/components/NavigationBar/NavigationBar.jsx
+++ my-app/src/components/NavigationBar/NavigationBar.jsx
@@ -1,4 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import PageLocation from '@/components/helpers/pageLocation'
+import { useCatalogApiStore } from '@/stores/CatalogApi';
+
 import IMAGES from '@/res/images'
+
 const {
     HomeNavIcon,
     HomeActiveNavIcon,
@@ -12,30 +20,58 @@ const {
     ChatActiveNavIcon,
 } = IMAGES;
 
-import React from 'react';
-import { StyleSheet, View, Image, Text } from 'react-native';
-
 export default function NavbarItem({ focused, routeName }) {
+    const { isRealtor } = PageLocation();
+    const navigation = useNavigation();
+
+    const { setCatalog } = useCatalogApiStore();
+
     let icon
+    const iconSize = 16;
+
     if (routeName === 'Объявления') {
-        icon = !focused ? <HomeNavIcon width={16} height={16} /> : <HomeActiveNavIcon width={16} height={16} />
+        icon = !focused ? <HomeNavIcon width={iconSize} height={iconSize} /> : <HomeActiveNavIcon width={iconSize} height={iconSize} />
     } else if (routeName === 'Избранное') {
-        icon = !focused ? <HeartDefault width={16} height={16} /> : <HeartActive width={16} height={16} />
+        icon = !focused ? <HeartDefault width={iconSize} height={iconSize} /> : <HeartActive width={iconSize} height={iconSize} />
     } else if (routeName === 'Создать') {
-        icon = !focused ? <AddNavIcon width={16} height={16} /> : <AddActiveNavIcon width={16} height={16} />
+        icon = !focused ? <AddNavIcon width={iconSize} height={iconSize} /> : <AddActiveNavIcon width={iconSize} height={iconSize} />
     } else if (routeName === 'Подборки') {
-        icon = !focused ? <CollectionNavIcon width={16} height={16} /> : <CollectionActiveNavIcon width={16} height={16} />
+        icon = !focused ? <CollectionNavIcon width={iconSize} height={iconSize} /> : <CollectionActiveNavIcon width={iconSize} height={iconSize} />
     } else if (routeName === 'Чаты') {
-        icon = !focused ? <ChatNavIcon width={16} height={16} /> : <ChatActiveNavIcon width={16} height={16} />
+        icon = !focused ? <ChatNavIcon width={iconSize} height={iconSize} /> : <ChatActiveNavIcon width={iconSize} height={iconSize} />
     }
-    return <View style={styles.navbarTab}>
-        {icon}
-        <Text numberOfLines={1}
-            ellipsizeMode='clip'
-            style={{ fontSize: 10, width: '15%', marginTop: 3, color: focused ? '#0072db' : '#222' }}>
-            {routeName}
-        </Text>
-    </View>
+    const resetCatalog = () => {
+        return isRealtor !== true ?
+            (setCatalog('Home'), navigation.navigate('Home')
+            )
+            :
+            (setCatalog('HomePageRealtor'),
+                navigation.navigate('HomePageRealtor')
+            )
+    }
+    return (routeName !== 'Объявления' ?
+        <View
+            style={styles.navbarTab}
+        >
+            {icon}
+            <Text numberOfLines={1}
+                ellipsizeMode='clip'
+                style={{ fontSize: 10, width: '15%', marginTop: 3, color: focused ? '#0072db' : '#222' }}>
+                {routeName}
+            </Text>
+        </View> :
+        <TouchableOpacity
+            style={styles.navbarTab}
+            onPress={() => resetCatalog()} >
+            {icon}
+            <Text numberOfLines={1}
+                ellipsizeMode='clip'
+                style={{ fontSize: 10, width: '15%', marginTop: 3, color: focused ? '#0072db' : '#222' }}>
+                {routeName}
+            </Text>
+        </TouchableOpacity>
+
+    )
 }
 
 const styles = StyleSheet.create({
